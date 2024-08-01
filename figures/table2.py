de8b586773af72f7660aee52356f80bfab4d0d04
Niels Mündler <n.muendler@posteo.de>
2024-08-01 14:53:16 +0200

    Add libro and p@5

--- figures/table2.py
+++ figures/table2.py
@@ -1,71 +1,11 @@
 """
 Method vs Applicability, FtX, FtP and PtP
 """
-import functools
-from typing import Tuple
 
 import fire
 from pathlib import Path
-import json
 
-from constants import FAIL_TO_PASS, FAIL_TO_FAIL, PASS_TO_PASS
-
-
-def collect_reports(model_name, run_id, instance_log_path: Path):
-    run_path = instance_log_path / run_id / model_name
-    reports = {}
-    for dir in run_path.iterdir():
-        if not dir.is_dir():
-            continue
-        report = dir / "report.json"
-        if not report.exists():
-            continue
-        with open(report) as f:
-            report_data = json.load(f)
-        instance_id = dir.name
-        reports[instance_id] = report_data[instance_id]
-    return reports
-
-
-def applied_count(reports):
-    return sum(r["patch_successfully_applied"] for r in reports.values())
-
-def ftp_count(reports):
-    return sum(r["resolved"] for r in reports.values())
-
-def get_ftx(report_pred: dict[str, list[str]], report_base: dict[str, list[str]]) -> int:
-    """
-    Determine resolved status of an evaluation instance
-
-    """
-    base_f2p = set(report_base[FAIL_TO_PASS])
-    pred_f2p = set(report_pred[FAIL_TO_PASS])
-
-    base_f2f = set(report_base[FAIL_TO_FAIL])
-    pred_f2f = set(report_pred[FAIL_TO_FAIL])
-
-    added_ftx = pred_f2p.difference(base_f2p) | pred_f2f.difference(base_f2f)
-
-    return bool(added_ftx)
-
-def ftx_count(reports):
-    return sum(get_ftx(r["tests_pred"], r["tests_base"]) for r in reports.values() if "tests_pred" in r)
-
-
-def get_ptp(report_pred: dict[str, list[str]], report_base: dict[str, list[str]]) -> int:
-    """
-    Determine resolved status of an evaluation instance
-
-    """
-    base_p2p = set(report_base[PASS_TO_PASS])
-    pred_p2p = set(report_pred[PASS_TO_PASS])
-
-    added_ptp = pred_p2p.difference(base_p2p)
-
-    return bool(added_ptp)
-
-def ptp_count(reports):
-    return sum(get_ptp(r["tests_pred"], r["tests_base"]) for r in reports.values() if "tests_pred" in r)
+from .util import collect_reports, applied_count, ftx_count, ftp_count, ptp_count
 
 def main(instance_log_path: str = "./run_instance_swt_logs", total_instance_count: int = 300):
     instance_log_path = Path(instance_log_path)
@@ -74,7 +14,8 @@ def main(instance_log_path: str = "./run_instance_swt_logs", total_instance_coun
     methods = [
         ("gpt-4-1106-preview", "zsb__gpt-4-1106-preview__bm25_27k_cl100k__seed=0,temperature=0", r"\zsb"),
         ("gpt-4-1106-preview", "zsp__gpt-4-1106-preview__bm25_27k_cl100k__seed=0,temperature=0", r"\zsp"),
-        # TODO libro,
+        ("gpt-4-1106-preview", "libro_gpt-4-1106-preview__bm25_27k_cl100k__seed=1,temperature=0.7.jsonl", r"\libro", "libro", [1,2,3,4,5], Path("inference_output/gpt-4-1106-preview__libro__libro_gpt-4-1106-preview__bm25_27k_cl100k__seed={seed},temperature=0.7.jsonl__[1, 2, 3, 4, 5]__gpt-4-1106-preview__test__seed=0,temperature=0__test.jsonl")),
+        ("gpt-4-1106-preview", "libro_gpt-4-1106-preview__bm25_27k_cl100k__seed=1,temperature=0.7.jsonl", r"\pak", "p@k", [1,2,3,4,5]),
         ("gpt-4-1106-preview", "acr__gpt-4-1106-preview", r"\acr"),
         ("aider--gpt-4-1106-preview", "aider_gpt-4-1106-preview", r"\aider"),
         ("gpt4__SWE-bench_Lite__default_test_demo3__t-0.00__p-0.95__c-3.00__install-1", "swea__gpt-4-1106-preview", r"\swea"),
@@ -82,8 +23,8 @@ def main(instance_log_path: str = "./run_instance_swt_logs", total_instance_coun
     ]
 
     print(r"Method & {$\bc{A}$ \up{}} & {\ftx \up{}} & {\ftp \up{}} & {\ptp} \\")
-    for model, run_id, name, in methods:
-        reports = collect_reports(model, run_id, instance_log_path)
+    for model, run_id, name, *args in methods:
+        reports = collect_reports(model, run_id, instance_log_path, *args)
         applied = 100*applied_count(reports)/total_instance_count
         ftp = 100*ftp_count(reports)/total_instance_count
         ftx = 100*ftx_count(reports)/total_instance_count
---
+++ figures/util.py
@@ -0,0 +1,130 @@
+from collections import defaultdict
+from typing import Tuple, Literal
+
+from pathlib import Path
+import json
+
+from constants import FAIL_TO_PASS, FAIL_TO_FAIL, PASS_TO_PASS
+
+def _collect_reports(model_name, run_id, instance_log_path: Path):
+    run_path = instance_log_path / run_id / model_name
+    reports = {}
+    for dir in run_path.iterdir():
+        if not dir.is_dir():
+            continue
+        report = dir / "report.json"
+        if not report.exists():
+            continue
+        instance_id = dir.name
+        with open(report) as f:
+            report_data = json.load(f)[instance_id]
+        patch_path = instance_log_path / run_id / f"pred_post_{model_name}" / dir.name / "model_patch.diff"
+        if patch_path.exists():
+            with open(patch_path) as f:
+                report_data["patch"] = f.read()
+        reports[instance_id] = report_data
+    return reports
+
+def _collect_reports_multi(model_name, run_id_pattern, instance_log_path: Path, seeds: Tuple[int]):
+    all_reports = defaultdict(dict)
+    for seed in seeds:
+        run_id = run_id_pattern.format(seed=seed)
+        reports = collect_reports(model_name, run_id, instance_log_path)
+        for instance_id, report in reports.items():
+            all_reports[instance_id][seed] = report
+    return all_reports
+
+def _select_reports_libro(all_reports, libro_decision_file: Path):
+    with open(libro_decision_file) as f:
+        libro_decisions = [json.loads(l) for l in f.readlines()]
+    libro_decisions = {d["instance_id"]: d for d in libro_decisions}
+    reports = {}
+    for instance_id, i_reports in all_reports.items():
+        cluster_preferred = []
+        cluster_secondary = []
+        for seed, report in i_reports.items():
+            report["seed"] = seed
+            libro_decision = libro_decisions.get(f"{instance_id}_seed={seed}", {}).get("full_output", "no")
+            if "yes" in libro_decision.lower():
+                cluster_preferred.append(report)
+            else:
+                cluster_secondary.append(report)
+        if not cluster_preferred and not cluster_secondary:
+            best_case = [r for r in i_reports.values()][0]
+        else:
+            def get_patch_len(x):
+                if len(x["patch"]) is None:
+                    return float("inf")
+                return len(x["patch"])
+            if not cluster_preferred:
+                best_case = min(cluster_secondary, key=get_patch_len)
+            else:
+                best_case = min(cluster_preferred, key=get_patch_len)
+        reports[instance_id] = best_case
+    return reports
+
+def _select_reports_passatk(all_reports):
+    reports = {}
+    for instance_id, i_reports in all_reports.items():
+        for attribute_pref in ["resolved", "added_f2p", "coverage_delta_pred", "patch_successfully_applied"]:
+            for seed, report in i_reports.items():
+                if report[attribute_pref] > reports.get(instance_id, {}).get(attribute_pref, 0):
+                    reports[instance_id] = report
+                    break
+            if reports.get(instance_id) is not None:
+                break
+    return reports
+
+
+def collect_reports(model_name, run_id, instance_log_path: Path, mode: Literal["single", "libro", "p@k"] = "single", seeds: Tuple[int] = None, libro_decision_file: Path = None):
+    if mode == "single":
+        return _collect_reports(model_name, run_id, instance_log_path)
+    if seeds is None:
+        raise ValueError("seeds must be provided for mode 'libro' or 'p@k'")
+    all_reports = _collect_reports_multi(model_name, run_id, instance_log_path, seeds)
+    if mode == "p@k":
+        return _select_reports_passatk(all_reports)
+    if mode == "libro":
+        if libro_decision_file is None:
+            raise ValueError("libro_decision_file must be provided for mode 'libro'")
+        return _select_reports_libro(all_reports, libro_decision_file)
+
+def applied_count(reports):
+    return sum(r["patch_successfully_applied"] for r in reports.values())
+
+def ftp_count(reports):
+    return sum(r["resolved"] for r in reports.values())
+
+def get_ftx(report_pred: dict[str, list[str]], report_base: dict[str, list[str]]) -> int:
+    """
+    Determine resolved status of an evaluation instance
+
+    """
+    base_f2p = set(report_base[FAIL_TO_PASS])
+    pred_f2p = set(report_pred[FAIL_TO_PASS])
+
+    base_f2f = set(report_base[FAIL_TO_FAIL])
+    pred_f2f = set(report_pred[FAIL_TO_FAIL])
+
+    added_ftx = pred_f2p.difference(base_f2p) | pred_f2f.difference(base_f2f)
+
+    return bool(added_ftx)
+
+def ftx_count(reports):
+    return sum(get_ftx(r["tests_pred"], r["tests_base"]) for r in reports.values() if "tests_pred" in r)
+
+
+def get_ptp(report_pred: dict[str, list[str]], report_base: dict[str, list[str]]) -> int:
+    """
+    Determine resolved status of an evaluation instance
+
+    """
+    base_p2p = set(report_base[PASS_TO_PASS])
+    pred_p2p = set(report_pred[PASS_TO_PASS])
+
+    added_ptp = pred_p2p.difference(base_p2p)
+
+    return bool(added_ptp)
+
+def ptp_count(reports):
+    return sum(get_ptp(r["tests_pred"], r["tests_base"]) for r in reports.values() if "tests_pred" in r)
