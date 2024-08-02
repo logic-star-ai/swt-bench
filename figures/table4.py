ec3e2ca0607086cb110037ab23381d3cc691047c
Niels Mündler <n.muendler@posteo.de>
2024-08-02 17:39:39 +0200

    Use tabulate

--- figures/table4.py
+++ figures/table4.py
@@ -7,8 +7,9 @@ import fire
 from pathlib import Path
 
 from figures.util import *
+from tabulate import tabulate
 
-def main(instance_log_path: str = "./run_instance_swt_logs", total_instance_count: int = 279):
+def main(instance_log_path: str = "./run_instance_swt_logs", total_instance_count: int = 279, format: str = "github"):
     instance_log_path = Path(instance_log_path)
     if not instance_log_path.exists():
         raise FileNotFoundError(f"Instance log directory not found at {instance_log_path}")
@@ -21,14 +22,20 @@ def main(instance_log_path: str = "./run_instance_swt_logs", total_instance_coun
         ("mistral-large__SWE-bench_Lite__default_test_demo3__t-0.00__p-0.95__c-3.00__install-1", "swea__mistral_large", r"Mistral Large 2"),
     ]
 
-    print(r"Method & {$\bc{A}$ \up{}} & {\ftx \up{}} & {\ftp \up{}} & {\ptp} \\")
+    headers = (
+        ["Method", r"{$\bc{A}}$ \up{}}", r"{\ftx \up{}}", r"{\ftp \up{}}", r"{\ptp}"]
+        if format == "latex" else
+        ["Method", "Applicability", "FtX", "FtP", "PtP", "Cost"]
+    )
+    rows = []
     for model, run_id, name, *args in methods:
         reports = collect_reports(model, run_id, instance_log_path, *args)
         applied = 100*no_error_count(reports)/total_instance_count
         ftp = 100*ftp_count(reports)/total_instance_count
         ftx = 100*ftx_count(reports)/total_instance_count
         ptp = 100*ptp_count(reports)/total_instance_count
-        print(rf"{name} & {applied:.1f} & {ftx:.1f} & {ftp:.1f} & {ptp:.1f} \\")
+        rows.append([name, f"{applied:.1f}", f"{ftx:.1f}", f"{ftp:.1f}", f"{ptp:.1f}"])
+    print(tabulate(rows, headers=headers, tablefmt=format))
 
 if __name__ == "__main__":
     fire.Fire(main)
