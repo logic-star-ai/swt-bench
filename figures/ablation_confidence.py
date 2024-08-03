b4ee24beaad796c0fc457cd1d58442692aebce4c
Niels Mündler <n.muendler@posteo.de>
2024-08-04 00:41:52 +0200

    Fix

--- figures/ablation_confidence.py
+++ figures/ablation_confidence.py
@@ -18,7 +18,7 @@ def main(instance_log_path: str = "./run_instance_swt_logs", total_instance_coun
     methods = [
         ("gpt-4o-mini-2024-07-18", "gpt-4o-mini-2024-07-18__swt_bench_lite_aug1_bm25_27k_cl100k_selfmade__seed=0,temperature=0.7,n=25__test._{seed}", r"\zsp 0.7", [0,1,2,3,4]),
         ("gpt-4o-mini-2024-07-18", "gpt-4o-mini-2024-07-18__swt_bench_lite_aug1_bm25_27k_cl100k_selfmade__seed=0,temperature=0.7,n=25__test._{seed}", r"\pak 0.7", [f"{{seed[{i}]}}" for i in range(5)], "p@k", ((0,1,2,3,4), (5,6,7,8,9), (10,11,12,13,14), (15,16,17,18,19), (20,21,22,23,24))),
-        ("gpt4o-mini__SWE-bench_Lite__default_test_demo4__t-0.70__p-0.95__s-5__c-3.00__install-1", "swe-agent-demo4-gpt4omini__swt_bench_lite__test__{seed}", r"\sweap 0.7", [1,2,3,4,5]),
+        ("gpt4o-mini__SWE-bench_Lite__default_test_demo4__t-0.70__p-0.95__s-{seed}__c-3.00__install-1", "swe-agent-demo4-gpt4omini__swt_bench_lite__test__{seed}", r"\sweap 0.7", [1,2,3,4,5]),
         ("aider_gpt4omini__swt-lite__test_s{seed}", r"\aider 0.7", [0,1,2,3,4]),
     ]
 
@@ -34,7 +34,7 @@ def main(instance_log_path: str = "./run_instance_swt_logs", total_instance_coun
         ptp_scores = []
         applied_scores = []
         for seed in seeds:
-            reports = collect_reports(model, run_id.format(seed=seed), instance_log_path, *args)
+            reports = collect_reports(model.format(seed=seed), run_id.format(seed=seed), instance_log_path, *args)
             applied_scores.append(100*no_error_count(reports)/total_instance_count)
             ftp_scores.append(100*ftp_count(reports)/total_instance_count)
             ftx_scores.append(100*ftx_count(reports)/total_instance_count)
