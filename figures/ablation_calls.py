09ce88122a321bcc222ae20de2a38f67631b1753
Niels Mündler <n.muendler@posteo.de>
2024-08-05 13:20:26 +0200

    Fix

--- figures/ablation_calls.py
+++ figures/ablation_calls.py
@@ -10,13 +10,13 @@ from pathlib import Path
 from figures.util import *
 
 
-def main(instance_log_path: str = "./run_instance_swt_logs", total_instance_count: int = 279, format="github", max_turns=20):
+def main(instance_log_path: str = "./run_instance_swt_logs", total_instance_count: int = 279, format="github", turns:list[int]=[2,3,5,10,15,20]):
     instance_log_path = Path(instance_log_path)
     if not instance_log_path.exists():
         raise FileNotFoundError(f"Instance log directory not found at {instance_log_path}")
     methods = [
-        ("gpt4__SWE-bench_Lite__default_test_demo3__t-0.00__p-0.95__c-3.00__install-1", "swea__gpt-4-1106-preview", r"\swea", "inference_outputs/gpt-4__swe_bench_lite_demo3_turns.jsonl"),
-        ("gpt4__SWE-bench_Lite__default_test_demo4__t-0.00__p-0.95__c-3.00__install-1", "sweap__gpt-4-1106-preview", r"\sweap", "inference_outputs/gpt-4__swe_bench_lite_demo4_turns.jsonl"),
+        ("gpt4__SWE-bench_Lite__default_test_demo3__t-0.00__p-0.95__c-3.00__install-1", "swea__gpt-4-1106-preview", r"\swea", "inference_output/gpt4__swe-bench_lite_demo3_turns.jsonl"),
+        ("gpt4__SWE-bench_Lite__default_test_demo4__t-0.00__p-0.95__c-3.00__install-1", "sweap__gpt-4-1106-preview", r"\sweap", "inference_output/gpt4__swe-bench_lite_demo4_turns.jsonl"),
     ]
 
     headers = (
@@ -30,7 +30,7 @@ def main(instance_log_path: str = "./run_instance_swt_logs", total_instance_coun
             num_turns = [json.loads(l) for l in (f)]
         num_turns_per_instance = {x["instance_id"]: x["num_turns"] for x in num_turns}
         reports = collect_reports(model, run_id, instance_log_path, *args)
-        for i in range(1, max_turns+1, max_turns//5):
+        for i in turns:
             reports_i = {k: v for k, v in reports.items() if num_turns_per_instance[k] <= i}
             applied = 100*no_error_count(reports_i)/total_instance_count
             ftp = 100*ftp_count(reports_i)/total_instance_count
