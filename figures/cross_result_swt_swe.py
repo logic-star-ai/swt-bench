5f9c593a6c81f8129924e044c09405ef146c39c8
Niels Mündler <n.muendler@posteo.de>
2024-08-06 20:02:14 +0200

    Add script to generate the cross validation dataset

--- figures/cross_result_swt_swe.py
+++ figures/cross_result_swt_swe.py
@@ -17,10 +17,11 @@ def main(swe_bench_results: str = "results/experiments-swe-bench/20240402_sweage
 
     swt_bench_reports = collect_reports("gpt4__SWE-bench_Lite__default_test_demo4__t-0.00__p-0.95__c-3.00__install-1", "sweap__gpt-4-1106-preview_reduced", instance_log_path)
     resolved_own_test = ftp_reports(swt_bench_reports)
-    swe_bench_accept_own = [r for r in swe_bench_applied if r["instance_id"] in resolved_own_test]
-    swe_bench_resolve_own = [r for r in swe_bench_resolved if r["instance_id"] in resolved_own_test]
+    swe_bench_accept_own = [r for r in swe_bench_applied if r in resolved_own_test]
+    swe_bench_resolve_own = [r for r in swe_bench_resolved if r in resolved_own_test]
     print("Own precision", len(swe_bench_resolve_own) / len(swe_bench_accept_own))
     print("Own filtered out ratio", len(swe_bench_accept_own) / len(swe_bench_applied))
+    print("Own recall", len(swe_bench_resolve_own) / len(swe_bench_resolved))
 
 if __name__ == '__main__':
     fire.Fire(main)
