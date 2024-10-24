57b7168f53d48740b3877fa10193e6aabf73c202
Niels <niels.muendler@inf.ethz.ch>
2024-10-24 10:55:25 +0200

    Add other ftp to ablation

--- figures/ablation_temperature.py
+++ figures/ablation_temperature.py
@@ -23,12 +23,13 @@ def main(instance_log_path: str = "./run_instance_swt_logs", total_instance_coun
     gold_reports = collect_reports(gold_model, gold_run_id, instance_log_path)
 
     headers = (
-        ["$T$", r"{$\bc{A}$ \up{}}", r"{\ftx \up{}}", r"{\ftp \up{}}", r"{\ptp}", r"{$\dc^{\text{all}}$ }"]
+        ["$T$", r"{$\bc{A}$ \up{}}", r"{\suc \up{}}", r"{\ftx \up{}}", r"{\ftp \up{}}", r"{\ptp}", r"{$\dc^{\text{all}}$ }"]
         if format.startswith("latex") else
-        ["Temperature", "Applicability", "F2X", "F2P", "P2P", "Coverage"]
+        ["Temperature", "Applicability", "Success", "F2X", "F2P", "P2P", "Coverage"]
     )
     rows = []
     for model, run_id, name, seeds, *args in methods:
+        actual_ftp_scores = []
         ftp_scores = []
         ftx_scores = []
         ptp_scores = []
@@ -37,6 +38,7 @@ def main(instance_log_path: str = "./run_instance_swt_logs", total_instance_coun
         for seed in seeds:
             reports = collect_reports(model, run_id.format(seed=seed), instance_log_path, *args)
             applied_scores.append(100*no_error_count(reports)/total_instance_count)
+            actual_ftp_scores.append(100*actual_ftp_count(reports)/total_instance_count)
             ftp_scores.append(100*ftp_count(reports)/total_instance_count)
             ftx_scores.append(100*ftx_count(reports)/total_instance_count)
             ptp_scores.append(100*ptp_count(reports)/total_instance_count)
@@ -46,13 +48,14 @@ def main(instance_log_path: str = "./run_instance_swt_logs", total_instance_coun
 
         if len(seeds) > 1:
             applied_mean, applied_ci = with_error_bars(applied_scores)
+            actual_ftp_mean, actual_ftp_me = with_error_bars(actual_ftp_scores)
             ftp_mean, ftp_me = with_error_bars(ftp_scores)
             ftx_mean, ftx_me = with_error_bars(ftx_scores)
             ptp_mean, ptp_me = with_error_bars(ptp_scores)
             coverages_delta_mean, coverages_delta_me = with_error_bars(coverages_delta)
-            rows.append([name, f"{applied_mean:.1f} ± {applied_ci:.1f}", f"{ftx_mean:.1f} ± {ftx_me:.1f}", f"{ftp_mean:.1f} ± {ftp_me:.1f}", f"{ptp_mean:.1f} ± {ptp_me:.1f}", f"{coverages_delta_mean:.1f} ± {coverages_delta_me:.1f}"])
+            rows.append([name, f"{applied_mean:.1f} ± {applied_ci:.1f}", f"{ftp_mean:.1f} ± {ftp_me:.1f}", f"{ftx_mean:.1f} ± {ftx_me:.1f}", f"{actual_ftp_mean:.1f} ± {actual_ftp_me:.1f}", f"{ptp_mean:.1f} ± {ptp_me:.1f}", f"{coverages_delta_mean:.1f} ± {coverages_delta_me:.1f}"])
         else:
-            rows.append([name, f"{applied_scores[0]:.1f}", f"{ftx_scores[0]:.1f}", f"{ftp_scores[0]:.1f}", f"{ptp_scores[0]:.1f}", f"{coverages_delta[0]:.1f}"])
+            rows.append([name, f"{applied_scores[0]:.1f}", f"{ftp_scores[0]:.1f}", f"{ftx_scores[0]:.1f}", f"{actual_ftp_scores[0]:.1f}", f"{ptp_scores[0]:.1f}", f"{coverages_delta[0]:.1f}"])
     print(tabulate(rows, headers=headers, tablefmt=format, floatfmt=".1f"))
 
 if __name__ == "__main__":
