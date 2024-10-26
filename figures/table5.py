1441420838629d2224276371cdbbad89368e04f6
Niels <niels.muendler@inf.ethz.ch>
2024-10-26 15:59:32 +0200

    Add new f2p score for table5

--- figures/table5.py
+++ figures/table5.py
@@ -31,17 +31,18 @@ def main(instance_log_path: str = "./run_instance_swt_logs"):
     gold_reports = collect_reports("gold", "validate-gold-1", instance_log_path)
     total_coverage_possible = count_coverage_delta_gold(gold_reports)
 
-    print(r"Method & {$\bc{A}$ \up{}} & {\suc \up{}} & {\ftx \up{}} & {$\dc^{all}$} \\")
+    print(r"Method & {$\bc{A}$ \up{}} & {\suc \up{}} & {\ftx \up{}} & {\ftp \up{}} & {$\dc^{all}$} \\")
     for model, run_id, name, *args in methods:
         reports = collect_reports(model, run_id, instance_log_path, *args)
         reports = {i: r for i, r in reports.items() if i in instances_with_invalid_patch}
         reports = no_error_reports(reports)
         applied = 100*no_error_count(reports)/total_instance_count
         ftp = 100*ftp_count(reports)/total_instance_count
+        actual_ftp = 100*actual_ftp_count(reports)/total_instance_count
         ftx = 100*ftx_count(reports)/total_instance_count
         ptp = 100*ptp_count(reports)/total_instance_count
         total_coverage_delta = 100 * sum_coverage_delta(reports) / total_coverage_possible
-        print(rf"{name} & {applied:.1f} & {ftp:.1f} & {ftx:.1f} & {total_coverage_delta:.1f} \\")
+        print(rf"{name} & {applied:.1f} & {ftp:.1f} & {ftx:.1f} & {actual_ftp:.1f} & {total_coverage_delta:.1f} \\")
 
 if __name__ == "__main__":
     fire.Fire(main)
