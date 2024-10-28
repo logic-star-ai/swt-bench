ec679d2c6792abece47fffc5644b3e7f5a54d25a
Niels <niels.muendler@inf.ethz.ch>
2024-10-28 14:44:29 +0100

    FIx

--- figures/venn_methods.py
+++ figures/venn_methods.py
@@ -6,6 +6,13 @@ import fire
 from figures.util import *
 
 from venny4py.venny4py import *
+from matplotlib import pyplot as plt
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "Computer Modern Roman",
+    "font.size": 20,
+})
 
 
 
@@ -24,11 +31,14 @@ def main(instance_log_path: str = "./run_instance_swt_logs", out_path: str = "fi
         reports = collect_reports(model, run_id, instance_log_path, *args)
         resolved_reports, _ = filtered_by_resolved(reports)
         sets[name] = set(resolved_reports.keys())
+    # note change bbox_to_anchor to bbox_to_anchor=(.5, 1.05)
     venny4py(
         sets,
         out_path,
         edge_color=None,
-        ext="pdf"
+        ext="pdf",
+        size=10,
+        font_size=30,
     )
     print("Total solved:", len(set.union(*sets.values())))
 
