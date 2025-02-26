a67a8dc21b602d01155ddbf7cbea37cfb3d1d4f0
Niels <niels.muendler@inf.ethz.ch>
2025-02-26 09:44:33 +0100

    Port log parser fix

--- src/log_parsers.py
+++ src/log_parsers.py
@@ -123,7 +123,8 @@ def parse_log_pytest_v2(log: str) -> dict[str, str]:
         # Support older pytest versions by checking if the line ends with the test status
         elif any([line.endswith(x.value) for x in TestStatus]):
             test_case = line.split()
-            test_status_map[test_case[0]] = test_case[1]
+            if len(test_case) >= 2:
+                test_status_map[test_case[0]] = test_case[1]
     return test_status_map
 
 
